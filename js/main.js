70f7a792cdbaeb11221438253800d766012ac564
Shahadat Shuvo <shuvombm02@gmail.com>
2017-03-02 22:50:05 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+      //counter-up
+         $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery)); 
